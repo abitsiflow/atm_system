04a50d36d4e783fbe98c7cae53b52e3ec932d8a7
abitsiflow <xtiangzmen@gmail.com>
2026-08-10 18:36:39 +0800

    [feat] added loop function for repeated attempts for testing

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,37 +5,42 @@ public class Main {
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
         Function func = new Function();
+        boolean mainIsRunning = true;
 
-        System.out.println("------------");
-        System.out.println("ATM MACHINE");
-        System.out.println("------------");
-        System.out.println("1. Deposit");
-        System.out.println("2. Withdraw");
-        System.out.println("3. Show Balance");
-        System.out.println("4. Exit");
-        System.out.print("Enter Choice(1-4): ");
-        int choice = scan.nextInt();
+        while(mainIsRunning){
+            System.out.println("------------");
+            System.out.println("ATM MACHINE");
+            System.out.println("------------");
+            System.out.println("1. Deposit");
+            System.out.println("2. Withdraw");
+            System.out.println("3. Show Balance");
+            System.out.println("4. Exit");
+            System.out.print("Enter Choice(1-4): ");
+            int choice = scan.nextInt();
 
-        switch(choice){
-            case 1:
-                System.out.println("deposit");
-                break;
+            switch(choice){
+                case 1:
+                    func.deposit(scan);
+                    break;
 
-            case 2:
-                System.out.println("withdraw");
-                break;
+                case 2:
+                    func.withdraw(scan);
+                    break;
 
-            case 3:
-                func.showBalance();
-                break;
+                case 3:
+                    func.showBalance();
+                    break;
 
-            case 4:
-                System.out.println("exit");
-                break;
-            default:
-                System.out.println("Invalid choice you've choose please try again!");
-                break;
+                case 4:
+                    System.out.println("exit");
+                    break;
+                default:
+                    System.out.println("Invalid choice you've choose please try again!");
+                    break;
+            }
+
         }
+
 
     }
 }
