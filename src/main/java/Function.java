b7885d27cf4c28600a5b7b7a61ef422ab6e88350
abitsiflow <xtiangzmen@gmail.com>
2026-08-10 18:35:15 +0800

    [feat] added withdraw function

--- src/main/java/Function.java
+++ src/main/java/Function.java
@@ -2,13 +2,13 @@ import java.util.Scanner;
 
 public class Function {
 
-    double balance = 0;
+    private double balance = 0;
 
     public void showBalance(){
         System.out.println("$: " + balance);
     }
 
-    public int deposit(Scanner scan){
+    public double deposit(Scanner scan){
         System.out.print("Enter a deposit: ");
         int amount = scan.nextInt();
 
@@ -17,9 +17,26 @@ public class Function {
             return 0;
         }else{
             System.out.println("Deposited succesfully!");
+            balance += amount;
+            return amount;
+        }
 
+    }
+
+    public double withdraw(Scanner scan){
+        System.out.print("Enter a Withdraw: ");
+        int amount = scan.nextInt();
+
+        if(amount < 0){
+            System.out.println("Withdraw cannot be negative or less than 0");
+            return 0;
+        }else if(amount > balance){
+            System.out.println("insufficient funds");
+            return 0;
+        }else {
+            System.out.println("Withdraw succefully!");
+            balance -= amount;
             return amount;
         }
     }
-
 }
