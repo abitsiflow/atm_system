19dff78b26e42f5bb04d260a5766739130cea777
abitsiflow <xtiangzmen@gmail.com>
2026-08-10 18:27:09 +0800

    [feat] added deposit function

--- src/main/java/Function.java
+++ src/main/java/Function.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
-public class function {
+public class Function {
+
+    double balance = 0;
+
+    public void showBalance(){
+        System.out.println("$: " + balance);
+    }
+
+    public int deposit(Scanner scan){
+        System.out.print("Enter a deposit: ");
+        int amount = scan.nextInt();
+
+        if(amount < 0){
+            System.out.println("Deposit cannot be negative or less than 0");
+            return 0;
+        }else{
+            System.out.println("Deposited succesfully!");
+
+            return amount;
+        }
+    }
+
 }
