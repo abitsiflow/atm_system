e6d68391f611400bd3bd75ff8a9e62ce70b33357
abitsiflow <xtiangzmen@gmail.com>
2026-08-28 21:15:18 +0800

    [chore] addiung Override

--- src/main/java/Transaction.java
+++ src/main/java/Transaction.java
@@ -1,5 +1,6 @@
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Formatter;
 
 public class Transaction {
     LocalDate date;
@@ -10,5 +11,9 @@ public class Transaction {
         this.date = LocalDate.now();
         this.type = type;
         this.amount = amount;
+    }
+    @Override
+    public string ToString(){
+        return;
     }
 }
