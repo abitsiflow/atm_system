05692ea2dbe7505ad106e2463836cd9500410745
abitsiflow <xtiangzmen@gmail.com>
2026-08-28 12:27:10 +0800

    [chore] added some code on line 9 to line 14

--- src/main/java/Transaction.java
+++ src/main/java/Transaction.java
@@ -2,5 +2,13 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class Transaction {
+    LocalDate date;
+    String type;
+    double amount;
 
+    public Transaction(String type, double amount){
+        this.date = LocalDate.now();
+        this.type = type;
+        this.amount = amount;
+    }
 }
